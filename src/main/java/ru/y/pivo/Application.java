684386bebbebc65e99640bbb3c170060225b922e
jubick1337 <mattyson.so@gmail.com>
2019-10-27 20:13:22 +0500

    Added front

--- src/main/java/ru/y/pivo/Application.java
+++ src/main/java/ru/y/pivo/Application.java
@@ -2,12 +2,26 @@ package ru.y.pivo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.servlet.ViewResolver;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Bean
+    public ViewResolver getViewResolver(ResourceLoader resourceLoader) {
+        MustacheViewResolver mustacheViewResolver
+                = new MustacheViewResolver();
+        mustacheViewResolver.setPrefix("templates/");
+        mustacheViewResolver.setSuffix(".mustache");
+        mustacheViewResolver.setCache(false);
+
+        return mustacheViewResolver;
+    }
 }
---
+++ src/main/java/ru/y/pivo/MainController.java
@@ -0,0 +1,24 @@
+package ru.y.pivo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index(Map<String, Object> model) {
+        //model.put("some", "hello, letsCode!");
+        return "index";
+    }
+}
