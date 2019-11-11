6b730b4285a6cdd29f104c243e3d4ebafb595410
WeedLord228 <zajtzevar@gmail.com>
2019-11-11 18:43:30 +0500

    Spring seurity with in memeory authorization

--- src/main/java/ru/y/pivo/MainController.java
+++ src/main/java/ru/y/pivo/MainController.java
@@ -13,12 +13,17 @@ public class MainController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        return "index";
+        return "home";
     }
 
     @GetMapping("/index")
     public String index(Map<String, Object> model) {
         //model.put("some", "hello, letsCode!");
         return "index";
+    }
+    @GetMapping("/maps")
+    public String maps(Map<String, Object> model) {
+        //model.put("some", "hello, letsCode!");
+        return "maps";
     }
 }
